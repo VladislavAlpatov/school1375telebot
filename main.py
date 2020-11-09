12a6843e10fac098d7a545c67f5fa163beae34c0
Vladislav <alpatof74@gmail.com>
2020-11-09 20:04:43 +0300

    апдейт

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class RangeNumberInLineButton(types.InlineKeyboardMarkup):
             self.add(types.InlineKeyboardButton(text=str(number), callback_data=str(number)))
 
 
+
 class RangeNumberReplyButton(types.ReplyKeyboardMarkup):
     def __init__(self, numbers):
         super().__init__()
