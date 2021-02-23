cb1ab37e77569e33c6f1e4524578e6fe408ec49a
Владислав Алпатов <alpatof74@gmail.com>
2021-02-23 16:51:09 +0300

    фикс бага с назывнием класс

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import os
 # Bot(os.environ.get('TOKEN'), os.environ.get('OWN_TOKEN')).run()
 
 if __name__ == '__main__':
-    bot.SchoolBot(os.environ.get('TOKEN'), os.environ.get('OWN_TOKEN')).run()
+    bot.Bot(os.environ.get('TOKEN'), os.environ.get('OWN_TOKEN')).run()
